dcf7f5fbc0f9d7b63aa3cb0d5011021c06d50b38
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2021-11-19 23:47:11 +0100

    Add list of tasks

--- scripts/Todo.js
+++ scripts/Todo.js
@@ -1,9 +1,11 @@
 class Todo {
     constructor() {
+        this.taskList = ['Task 1', 'Task 2', 'Task 3']
         this.render()
     }
     render() {
         this.addFormForAddingTask()
+        this.addListOFTasks()
     }
     addFormForAddingTask() {
         const input = document.createElement('input')
@@ -11,5 +13,13 @@ class Todo {
         button.textContent = 'Add task'
         document.body.appendChild(input)
         document.body.appendChild(button)
+    }
+    addListOFTasks() {
+        const ul = document.createElement('ul')
+        document.body.appendChild(ul)
+        this.taskList.length === 0 ? ul.innerText = 'There are no added tasks in this category' : this.taskList.forEach(task => {
+            const li = document.createElement('li')
+            ul.appendChild(li).innerText = task
+        })
     }
 }
