4f303ff662add4e7435bb68daeb0816cd5678348
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2021-11-21 23:28:58 +0100

    Add list methods

--- scripts/Element.js
+++ scripts/Element.js
@@ -35,9 +35,43 @@ class Input extends Element {
     }
 }
 class List extends Element {
-    constructor(_setClass) {
+    constructor(_setClass, _action) {
         super(_setClass)
         this.tag = 'ul'
+        this.tasksList = []
         this.create(this.tag)
+        this.addNewTask('taskAddButton', 'taskAddInput')
+        this.render()
+    }
+    render() {
+        this.tasksList.length === 0 ? this.emptyTaskList() : this.tasksList.forEach(element, () => {
+            console.log('element')
+        })
+
+    }
+    emptyTaskList() {
+        const tasksList = document.querySelector(`.taskList`)
+        tasksList.innerText = "There is no tasks"
+    }
+    addNewTask(selectButton, selectInput) {
+        const button = document.querySelector(`.${selectButton}`)
+        const input = document.querySelector(`.${selectInput}`)
+        button.addEventListener('click', () => {
+            input.value === '' ? alert('Please enter the task') : this.tasksList.push({
+                value: input.value,
+                date: new Date
+            })
+            input.value = ''
+            this.render()
+        })
+
+    }
+}
+class Task extends Element {
+    constructor(_setClass, _value) {
+        super(_setClass)
+        this.tag = 'li'
+        this.create(this.tag)
+        this.setText(this.text)
     }
 }
--- scripts/Todo.js
+++ scripts/Todo.js
@@ -1,23 +1,7 @@
 class Todo {
     constructor() {
-        this.taskList = []
-        this.render()
-    }
-    render() {
         new Input('taskAddInput', 'Task value')
         new Button('taskAddButton', 'Add task')
-        this.addNewTask('taskAddButton', 'taskAddInput')
-        this.update()
-    }
-    update() {
         new List('taskList')
     }
-    addNewTask(selectButton, selectInput) {
-        const button = document.querySelector(`.${selectButton}`)
-        const input = document.querySelector(`.${selectInput}`)
-        button.addEventListener('click', () => {
-            input.value === '' ? alert('Please enter the task') : this.taskList.push(input.value)
-        })
-    }
-
 }
